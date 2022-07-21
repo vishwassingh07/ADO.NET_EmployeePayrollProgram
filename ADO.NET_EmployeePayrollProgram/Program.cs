4ea8a16a84a3684be5f4a145ddb3fe538e6439dc
Vishwas <baghel.vishwas0@gmail.com>
2022-07-21 23:51:05 +0530

    [Vishwas]add: Employee Payroll Program With ADO.NET Added

---
+++ ADO.NET_EmployeePayrollProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ADO.NET_EmployeePayrollProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To The Employee Payroll Program With ADO.NET ");
+        }
+    }
+}
